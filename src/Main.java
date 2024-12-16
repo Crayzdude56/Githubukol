7b6d14908965825ec0930c8f95ae727c57ac4051
slaoli56 <slaoli56@gasos-ro.cz>
2024-12-16 12:35:54 +0100

    githubukoly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Metoda ukol1 = new Metoda();
+
+        System.out.println("napis slovo");
+        String vypis = sc.nextLine();
+        System.out.print(vypis);
+
+
+        ukol1.ahojsvete();
+        ukol1.ukol3();
+        ukol1.nasobky5();
+        ukol1.ukol5();
+
     }
 }
